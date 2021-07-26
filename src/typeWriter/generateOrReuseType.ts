5379442fb669338bad3cd2d3db319026d3aa4d72
John Wright <jrite@protonmail.com>
2021-07-26 10:40:52 +0100

    refactor: simplify type name discovery

--- src/typeWriter/generateOrReuseType.ts
+++ src/typeWriter/generateOrReuseType.ts
@@ -1,14 +1,10 @@
-import { tryCatch } from '@johngw/error'
 import { Type } from 'ts-morph'
 import factory from './factory'
 import RuntypeGenerator from './RuntypeGenerator'
 import { Declare } from './symbols'
 
 export default function* generateOrReuseType(type: Type): RuntypeGenerator {
-  const typeName = tryCatch(
-    () => type.getSymbolOrThrow().getName(),
-    () => null
-  )
+  const typeName = type.getSymbol()?.getName()
 
   if (!!typeName && (yield [Declare, typeName])) {
     return
