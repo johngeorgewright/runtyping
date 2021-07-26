b58999d8f73809a3811c2a923cc23899a262ad5c
John Wright <jrite@protonmail.com>
2021-07-26 10:41:44 +0100

    refactor: restricting simpletypegenerator
    Make sure type is one of runtypes.

--- src/typeWriter/simple.ts
+++ src/typeWriter/simple.ts
@@ -1,7 +1,10 @@
 import RuntypeGenerator from './RuntypeGenerator'
 import { Import, Write } from './symbols'
+import * as runtypes from 'runtypes'
 
-export default function* simpleTypeGenerator(type: string): RuntypeGenerator {
+export default function* simpleTypeGenerator(
+  type: keyof typeof runtypes
+): RuntypeGenerator {
   yield [Import, type]
   yield [Write, type]
 }
