12f623cffd91cd121f22cbda8266ac522dad5933
John Wright <jrite@protonmail.com>
2021-09-13 14:32:15 +0100

    refactor: move all file manipulation in to 1 place
    A new Generator class to encapsulate all file manipulation.
    
    BREAKING CHANGE: generate() is deprecated in favor of Generator
    constructor.

--- test/generateFixture.ts
+++ test/generateFixture.ts
@@ -1,23 +1,15 @@
 import * as pathHelper from 'path'
-import generate from '../src/generate'
-import { accumulate } from '@johngw/async-iterator'
+import Generator from '../src/Generator'
 
 export default async function generateFixture(name: string, types: string[]) {
-  const [file] = await accumulate(
-    generate({
-      buildInstructions: [
-        {
-          targetFile: pathHelper.join(__dirname, `${name}.runtypes.ts`),
-          sourceTypes: [
-            {
-              file: pathHelper.join(__dirname, `${name}.ts`),
-              type: types,
-            },
-          ],
-        },
-      ],
-    })
-  )
+  const generator = new Generator({
+    targetFile: pathHelper.join(__dirname, `${name}.runtypes.ts`),
+  })
 
-  return file
+  return generator.generate([
+    {
+      file: pathHelper.join(__dirname, `${name}.ts`),
+      type: types,
+    },
+  ])
 }
