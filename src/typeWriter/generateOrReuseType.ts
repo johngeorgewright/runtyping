3e388f0c15acb9b7e6362b3bd5c59fd675d704af
John Wright <jrite@protonmail.com>
2022-02-02 14:19:15 +0000

    fix: using enum members within types
    See #202

--- src/typeWriter/generateOrReuseType.ts
+++ src/typeWriter/generateOrReuseType.ts
@@ -2,12 +2,17 @@ import { Type } from 'ts-morph'
 import factory from './factory'
 import TypeWriter from './TypeWriter'
 import { DeclareAndUse } from './symbols'
+import enumReferenceTypeGenerator from './enumReference'
 
 export default function* generateOrReuseType(type: Type): TypeWriter {
   const typeName =
     type.getAliasSymbol()?.getName() || type.getSymbol()?.getName()
 
-  if (!!typeName && (yield [DeclareAndUse, typeName])) return
+  if (!!typeName) {
+    if (type.isEnumLiteral())
+      return yield* enumReferenceTypeGenerator(type, typeName)
+    else if (yield [DeclareAndUse, typeName]) return
+  }
 
   yield* factory(type)
 }
--- test/enum.test.ts
+++ test/enum.test.ts
@@ -1,10 +1,10 @@
 import generateFixture from './generateFixture'
 
 test('enum', async () => {
-  expect((await generateFixture('enum', ['A', 'B'])).getText())
+  expect((await generateFixture('enum', ['A', 'B', 'D'])).getText())
     .toMatchInlineSnapshot(`
-    "import { A as _A, B as _B } from './enum';
-    import { Guard, Static } from 'runtypes';
+    "import { A as _A, B as _B, C as _C } from './enum';
+    import { Guard, Static, Literal } from 'runtypes';
 
     export const A = Guard((x: any): x is _A => Object.values(_A).includes(x));
 
@@ -13,6 +13,10 @@ test('enum', async () => {
     export const B = Guard((x: any): x is _B => Object.values(_B).includes(x));
 
     export type B = Static<typeof B>;
+
+    export const D = Literal(_C.A).Or(Literal(_C.B));
+
+    export type D = Static<typeof D>;
     "
   `)
 })
