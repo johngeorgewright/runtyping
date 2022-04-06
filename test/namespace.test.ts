59b56fe2d758f13b307b19a228f94a5ba9be906f
MrNerdHair <mrnerdhair@gmail.com>
2022-04-06 12:55:46 -0400

    test case for inter-namespace references

--- test/namespace.test.ts
+++ test/namespace.test.ts
@@ -2,7 +2,9 @@ import generateFixture from './generateFixture'
 
 test('namespace', async () => {
   expect(
-    (await generateFixture('namespace', ['A.B', 'B.C.D', 'A.C'])).getText()
+    (
+      await generateFixture('namespace', ['A.B', 'B.C.D', 'A.C', 'A.D'])
+    ).getText()
   ).toMatchInlineSnapshot(`
     "import { Record, String, Static, Number, Unknown } from 'runtypes';
 
@@ -14,6 +16,10 @@ test('namespace', async () => {
       export const C = Unknown;
 
       export type C = Static<typeof C>;
+
+      export const D = Record({ E: Number, });
+
+      export type D = Static<typeof D>;
     }
 
     export namespace B {
