e53b25bf6f6821d87ffe7cc255cd28e970be5660
John Wright <jrite@protonmail.com>
2022-05-04 19:04:50 +0100

    refactor: narrow simple types
    We now narrow the simple type argument to only those that are already
    runtypes and not functions that return runtypes.

--- src/typeWriter/simple.ts
+++ src/typeWriter/simple.ts
@@ -1,10 +1,18 @@
 import TypeWriter from './TypeWriter'
 import { Import, Write } from './symbols'
 import * as runtypes from 'runtypes'
+import { PickByValue } from '../util'
 
-export default function* simpleTypeWriter(
-  type: keyof typeof runtypes
-): TypeWriter {
+/**
+ * A runtype is considered "simple" when it is already a Runtype
+ * and not a function that returns a Runtype.
+ *
+ * For example, `Number` & `String` are simple types, but
+ * `Array` and `Record` are not.
+ */
+type SimpleRuntype = keyof PickByValue<typeof runtypes, runtypes.Runtype>
+
+export default function* simpleTypeWriter(type: SimpleRuntype): TypeWriter {
   yield [Import, type]
   yield [Write, type]
 }
