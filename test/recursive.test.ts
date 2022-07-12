5a76b169154b9acde33e50b0a30fbadd050d02a5
John Wright <jrite@protonmail.com>
2022-07-12 16:08:16 +0100

    test: update snapshots

--- test/recursive.test.ts
+++ test/recursive.test.ts
@@ -5,11 +5,11 @@ test('recursive', async () => {
     .toMatchInlineSnapshot(`
     "import { Lazy, Record, String, Static, Array } from 'runtypes';
 
-    export const A: Runtype<_A> = Lazy(() => Record({ recurse: String.Or(A), }));
+    export const A = Lazy(() => Record({ recurse: String.Or(A), }));
 
     export type A = Static<typeof A>;
 
-    export const B: Runtype<_B> = Lazy(() => Record({ recurse: Array(B), }));
+    export const B = Lazy(() => Record({ recurse: Array(B), }));
 
     export type B = Static<typeof B>;
     "
