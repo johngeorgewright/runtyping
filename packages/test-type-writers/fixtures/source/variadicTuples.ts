730c243bad61cbccef4a6f3b27074ffa6065f336
John Wright <jrite@protonmail.com>
2022-08-18 15:54:39 +0100

    feat: add new detailed variadic tuple test

--- packages/test-type-writers/fixtures/source/variadicTuples.ts
+++ packages/test-type-writers/fixtures/source/variadicTuples.ts
@@ -5,3 +5,5 @@ export type B = [string, number, ...number[]]
 export type C = [...string[], number]
 
 export type D = [string, ...string[], string]
+
+export type E = [string, number, boolean, ...string[], number, boolean]
