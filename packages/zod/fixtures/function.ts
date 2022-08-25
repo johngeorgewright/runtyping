f536f42d8621771a1c1bf5243126de5f2646ce5e
John Wright <jrite@protonmail.com>
2022-08-25 11:28:41 +0100

    feat(zod): better static imp of functions

--- packages/zod/fixtures/function.ts
+++ packages/zod/fixtures/function.ts
@@ -1,5 +1,22 @@
-import { function as func, infer as Infer, number, string, void as Void } from 'zod';
+import { function as func } from 'zod';
+import { A as _A, B as _B, C as _C, F as _F, G as _G } from '../../../.yarn/__virtual__/@runtyping-test-type-writers-virtual-f1a80c3a62/1/packages/test-type-writers/fixtures/source/function';
 
-export const A = func().args(string(), number(),).returns(Void());
+export const A = func();
 
-export type A = Infer<typeof A>;
+export type A = _A;
+
+export const B = func();
+
+export type B = _B;
+
+export const C = func();
+
+export type C = _C;
+
+export const F = func();
+
+export type F = typeof _F;
+
+export const G = func();
+
+export type G = typeof _G;
--- packages/zod/fixtures/interface.ts
+++ packages/zod/fixtures/interface.ts
@@ -1,4 +1,5 @@
-import { boolean, function as func, infer as Infer, literal, number, object, string, void as Void } from 'zod';
+import { boolean, function as func, infer as Infer, literal, number, object, string } from 'zod';
+import { boo as _boo, foo as _foo } from '../../../.yarn/__virtual__/@runtyping-test-type-writers-virtual-f1a80c3a62/1/packages/test-type-writers/fixtures/source/interface';
 
 export const A = object({ foo: string(), bar: number(), [`has spaces`]: boolean(), [`+1`]: boolean(), [`-1`]: boolean(), __underscores__: boolean(), $dollar: boolean(), [`\${escaped template vars}`]: boolean(), });
 
@@ -8,6 +9,6 @@ export const B = object({ a: A, b: literal("B"), });
 
 export type B = Infer<typeof B>;
 
-export const C = object({ foo: func().args().returns(string()), bar: number(), boo: func().args(string(),).returns(Void()), });
+export const C = object({ foo: func(), bar: number(), boo: func(), });
 
 export type C = Infer<typeof C>;
