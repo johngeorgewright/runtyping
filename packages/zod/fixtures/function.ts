24c9fc8d8fd86b5543fa12f84fc9ffe41fa20e77
John Wright <jrite@protonmail.com>
2022-08-25 10:00:08 +0100

    feat(zod): function declarations
    So that the static typed matches the function declaration.

--- packages/zod/fixtures/function.ts
+++ packages/zod/fixtures/function.ts
@@ -1,5 +1,5 @@
-import { function as func, infer as Infer } from 'zod';
+import { function as func, infer as Infer, number, string, void as Void } from 'zod';
 
-export const A = func();
+export const A = func().args(string(), number(),).returns(Void());
 
 export type A = Infer<typeof A>;
